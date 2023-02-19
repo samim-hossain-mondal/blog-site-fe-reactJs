46b91e3c2c9caeed5fa7a09988f5759136c4869c
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-02-19 23:39:13 +0530

    feat: built blog website

---
+++ src/assets/mockData/index.js
@@ -0,0 +1,58 @@
+const cardData = [
+  {
+    'date': '2nd Januray, 2018',
+    'readingTime': '2 mins',
+    'title': 'The future of abstract art and the culture ...',
+    'description': 'Create a blog post subtitle that summarizes your post in a few short, punchy sentences and entices your...',
+    'claps': 10,
+    'liked': false,
+    'image': 'https://static.wixstatic.com/media/178772d8bf024ceaa9dbf60aa317862c.jpg/v1/fill/w_1480,h_1208,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/178772d8bf024ceaa9dbf60aa317862c.jpg'
+  },
+  {
+    'date': '31st Januray, 2018',
+    'readingTime': '1 min',
+    'title': 'How painting outdoors can transform your ',
+    'description': 'Create a blog post subtitle that summarizes your post in a few short, punchy sentences and entices your...',
+    'claps': 1,
+    'liked': true,
+    'image': 'https://static.wixstatic.com/media/32d2d243df3a90d11ebc9a3c713530b2.jpg/v1/fill/w_859,h_979,al_c,q_85,enc_auto/32d2d243df3a90d11ebc9a3c713530b2.jpg'
+  },
+  {
+    'date': '20th March, 2018',
+    'readingTime': '2 mins',
+    'title': '10 young painters you need to know',
+    'description': 'Create a blog post subtitle that summarizes your post in a few short, punchy sentences and entices your...',
+    'claps': 15,
+    'liked': false,
+    'image': 'https://gloryartgallery.files.wordpress.com/2014/12/aliaa-13.jpg'
+  },
+  {
+    'date': '22nd June, 2018',
+    'readingTime': '5 mins',
+    'title': 'Tips for creating the perfect art',
+    'description': 'Create a blog post subtitle that summarizes your post in a few short, punchy sentences and entices your...',
+    'claps': 11,
+    'liked': false,
+    'image': 'https://www.animeignite.com/wp-content/uploads/2018/11/20181018_045922000_iOS-694x675.jpg'
+  },
+  {
+    'date': '4th Feburary, 2019',
+    'readingTime': '9 mins',
+    'title': 'Stay inspired: top artists blog ...',
+    'description': 'Create a blog post subtitle that summarizes your post in a few short, punchy sentences and entices your...',
+    'claps': 45,
+    'liked': true,
+    'image': 'https://developer-blogs.nvidia.com/wp-content/uploads/2022/01/AdobeStock_327477069.jpeg'
+  },
+  {
+    'date': '10th Januray, 2019',
+    'readingTime': '2 mins',
+    'title': '5 reasons why everyone should try an...',
+    'description': 'Create a blog post subtitle that summarizes your post in a few short, punchy sentences and entices your...',
+    'claps': 11,
+    'liked': false,
+    'image': 'https://static.wixstatic.com/media/31ade2aaec34237fc4467c3cf7d6e053.jpg/v1/fill/w_695,h_695,al_c,q_85,enc_auto/31ade2aaec34237fc4467c3cf7d6e053.jpg'
+  }
+];
+
+export default cardData;
